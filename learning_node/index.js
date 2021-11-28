ab9c2289e465759fb3e5efc248aea4ecb98de958
aleem aheer <aleemaheer5769644@gmail.com>
2021-11-28 23:14:58 +0500

    Updated

--- learning_node/index.js
+++ learning_node/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const path = require("path");
 const fs = require("fs");
 
-const server = http.createServer((req, res) => {
+/*const server = http.createServer((req, res) => {
     if (req.url === '/' || req.url === '/home') {
     fs.readFile(path.join(__dirname, 'public', 'home.html'), 
     (err, content) => {
@@ -29,7 +29,53 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { "Content-Type": "application/json"});
         res.end(JSON.stringify(users));
     };
-});
+});*/
+
+const server = http.createServer((req, res) => {
+    // Build file path
+    let file_path = path.join(__dirname, 'public', req.url === '/' ? 'home.html' : req.url);
+
+    // Check extension name
+    let extname = path.extname(file_path);
+
+    // Initial content type 
+    let content_type = 'index.html';
+    switch (extname) {
+        case '.js':
+            content_type = 'text/javascript';
+            break;
+        case '.css':
+            content_type = 'text/css';
+            break;
+        case '.html':
+            content_type = 'text/html'
+            break;
+        case '.json':
+            content_type = 'application/json'
+            break;     
+    }
+    fs.readFile(file_path, (err, content) => {
+        if (err) {
+            if (err.code = 'ENOENT') {
+                // Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type': content_type });
+                    res.end(content, 'utf8');
+                })
+            }
+            else {
+                // Some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        }
+        else {
+            // Success
+            res.writeHead(200, { 'Content-Type': content_type });
+            res.end(content, 'utf8');
+        }
+    })
+})
 
 const PORT = process.env.PORT || 5000;
 
